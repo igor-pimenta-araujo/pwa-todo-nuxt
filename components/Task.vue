9ddf7a71d1b1020ce44ce845037623943a5a4186
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2022-09-01 19:59:33 -0300

    funcao isdone pronta

--- components/Task.vue
+++ components/Task.vue
@@ -2,8 +2,8 @@
 <template>
     <div>
         <div v-for="task in tasks" class="flex mb-4 items-center">
-            <p class="w-full text-grey-darkest">{{task}}</p>
-            <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
+            <p v-bind:class="{  'line-through': task.done }" class="w-full text-grey-darkest ">{{task.description}}</p>
+            <button v-on:click="isDone(task)" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
             <button v-on:click="deleteTask(task)" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
         </div>
     </div>
@@ -16,6 +16,9 @@ export default {
   methods: {
     deleteTask(task){
          this.$emit("deleteTask", task);
+    },
+    isDone(task){
+        this.$emit("isDone", task);
     }
   }
 }
--- components/Todo.vue
+++ components/Todo.vue
@@ -14,7 +14,7 @@
                     <input v-model="task" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" v-on:keyup.enter="addTask(task)" placeholder="Add Todo">
                 </div>
             </div>
-            <Task v-bind:tasks="listTasks" v-on:deleteTask="deleteTask"></Task>
+            <Task v-bind:tasks="listTasks" v-on:deleteTask="deleteTask" v-on:isDone="isDone"></Task>
         </div>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
@@ -32,17 +32,30 @@ export default {
         return {
           task: "",
           message: "",
-          listTasks: ["igor"]
+          listTasks: [
+            {description: 'Learn Vue', done: false},
+          ]
         };
   },
   methods: {
     addTask(task){
-      this.listTasks.push(task);
+      let taskToRecord = {
+        description: task,
+        done: false
+      }
+      this.listTasks.push(taskToRecord);
       this.task = "";
     },
     deleteTask: function (task) {
 			this.listTasks.splice(this.listTasks.indexOf(task), 1);
     },
-  }
+    isDone: function(task) {
+      const toDoToUpdate = this.listTasks.find(item => item === task);
+      toDoToUpdate.done = !(toDoToUpdate.done)
+    },
+  },
+  created() {
+    console.log(this.ToDoItems);
+  },
 }
 </script>
