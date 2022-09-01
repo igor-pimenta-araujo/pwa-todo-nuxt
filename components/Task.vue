78302986dee930ea2bacfa3730755ccebe651076
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2022-08-31 21:15:59 -0300

    initial

---
+++ components/Task.vue
@@ -0,0 +1,17 @@
+<!-- Please remove this file from your project -->
+<template>
+    <div>
+        <div v-for="task in tasks" class="flex mb-4 items-center">
+            <p class="w-full text-grey-darkest">{{task}}</p>
+            <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>
+            <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: ['tasks']
+}
+</script>
---
+++ components/Todo.vue
@@ -0,0 +1,45 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <h1 class="text-center text-2xl">Todo List</h1>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold text-center">
+          Adicione itens a fazer
+        </h2>
+        <div>
+            <div class="mb-4">
+                <div class="flex mt-4">
+                    <input v-model="task" class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" v-on:keyup.enter="addTask(task)" placeholder="Add Todo">
+                </div>
+            </div>
+            <Task v-bind:tasks="listTasks"></Task>
+        </div>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
+        </p>
+      </div>
+      <Social></Social>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+        return {
+          task: "",
+          message: "",
+          listTasks: ["igor", "felipe"]
+        };
+  },
+  methods: {
+    addTask(task){
+      this.listTasks.unshift(task);
+      this.task = "";
+    }
+  },
+}
+</script>
